3cb3c128cfee35faba8f767f1057ae58e7e304e2
brewInstallMetroid <christopherthompso@unomaha.edu>
2026-01-22 11:39:57 -0600

    class notes taken for week 2

--- class/Notes2.java
+++ class/Notes2.java
@@ -1,8 +1,58 @@
+/* NOTES FOR WEEK TWO
+ * Basic Language Elements
+	 * if (<boolean exp>) {
+	 * 	<conditional statements>
+	 * }
+	 * if (<boolean exp>) {
+	 * 	<conditional statements>
+	 * } else {
+	 * `<conditional statements>
+	 * }
+ * 
+ *
+ *
+ * Classes in Java
+ *
+ *
+ * NOTE: labeled class
+ *
+ * class className {
+ * 		private int id; 		//instance variables
+ * 		private String name;	//instance variables
+ * 		
+ * 		public Constructor (int arg, int arg, etc.) {
+ * 			this.varname = var;
+ * 		}
+ * }
+ *
+ *
+ * NOTE: Course class from lab 2
+ *
+ * public class Course {
+ * 		private int id;
+ * 		private String name;
+ * 		private String title;
+ * 		private Student student;
+*
+ * 		public Course (Integer id, String name, String title) {
+ * 			this.id = id;
+ * 			this.name = name;
+ * 			this.title = title;
+ * 		}
+ *
+ * 		public void setStudent (Student s) {
+ *			this.student = s;
+ * 		}
+ * }
+ * 		
+ *
+ * NOTE:: The two classes above share something.  This is called an aggregation relationship.  The Course c:wq
+ * lass "Has a student".  Other relationships: "is a", "uses"
+ *
+ * */
 
 public class Notes2 {
 	public static void main() {
-		:w
-
 
 	}
 
